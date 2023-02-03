058f08e8f36714dce030a57df3a1e1ec4170e85d
Glenn 'devalias' Grant <glenn@devalias.net>
2023-02-03 11:19:24 +1100

    add leaked ChatGPT model to script + links about it already being taken down

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ async function main() {
     const openai = new OpenAIApi(configuration);
 
     const completion = await openai.createCompletion({
+      // TODO: Seems this leaked ChatGPT API model got taken down already..
+      //   https://github.com/acheong08/ChatGPT/issues/523
+      //     https://github.com/acheong08/ChatGPT/issues/523#issuecomment-1414524642
+      // model: "text-chat-davinci-002-20230126",
       model: "text-davinci-003",
       prompt: "Hello world",
     });
