ff4326ef739401f6f46678c5577e3ea8de880687
Glenn 'devalias' Grant <glenn@devalias.net>
2023-02-05 13:47:59 +1100

    refactor package.json script update-models into ./scripts/update_models.sh

---
+++ scripts/update_models.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+models_json='./models.json'
+models_txt='./models.txt'
+
+if [ -z "$OPENAI_API_KEY" ]; then
+  echo "Error: OPENAI_API_KEY is not set. Please run the following, then try again:"
+  echo "  export OPENAI_API_KEY='YOUR-API-KEY'"
+  exit 1
+fi
+
+echo "Updating ${models_json} / ${models_txt}"
+
+curl --silent https://api.openai.com/v1/models -H "Authorization: Bearer ${OPENAI_API_KEY}" > "${models_json}"
+
+jq -r '.data[].id' "${models_json}" > "${models_txt}"
+
+echo 'Done!'
