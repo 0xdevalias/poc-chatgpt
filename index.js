68b25f2cfcb2ef2c179d843f6a04c913f6bc5ab9
Glenn 'devalias' Grant <glenn@devalias.net>
2023-02-05 11:06:25 +1100

    update code to use recently found model: text-chat-davinci-002-20221122

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ async function main() {
       //   https://github.com/acheong08/ChatGPT/issues/523
       //     https://github.com/acheong08/ChatGPT/issues/523#issuecomment-1414524642
       // model: "text-chat-davinci-002-20230126",
-      model: "text-davinci-003",
-      prompt: "Hello world",
+      // Ref:
+      //   https://github.com/waylaidwanderer/node-chatgpt-api/issues/19#issuecomment-1415102926
+      //   https://github.com/acheong08/ChatGPT/issues/533#issuecomment-1414779079
+      model: "text-chat-davinci-002-20221122",
+      // model: "text-davinci-003",
+      prompt: "Are you ChatGPT?",
     });
     console.log(completion.data.choices[0].text);
 }
