1801401cff3753f4b10326feb9e26ca73dd05949
Glenn 'devalias' Grant <glenn@devalias.net>
2023-02-05 13:34:51 +1100

    update extract_models_by_owned_by.sh to extract models_*.txt for each group

--- scripts/extract_models_by_owned_by.sh
+++ scripts/extract_models_by_owned_by.sh
@@ -2,9 +2,17 @@
 
 models_json='./models.json'
 grouped_json='./models_by_owned_by.json'
+grouped_txt_prefix='./models_'
+grouped_txt_suffix='.txt'
 
 echo "Extracting models from ${models_json} and grouping them by owned_by"
 
 jq -r '.data | sort_by(-.created) | map({ id: .id, owned_by: .owned_by, created: .created, created_iso8601: (.created | todate) }) | group_by(.owned_by) | map({ (.[0].owned_by): . }) | add' "${models_json}" > "${grouped_json}"
 
+for group in $(jq -r 'keys[]' "${grouped_json}"); do
+  echo "  Extracting ${group}.."
+  jq -r ".[\"$group\"][] | \"\(.created_iso8601) \(.id)\"" "${grouped_json}" > "${grouped_txt_prefix}${group}${grouped_txt_suffix}"
+done
+
+echo "Extracted models saved to ${grouped_json} / ${grouped_txt_prefix}*${grouped_txt_suffix}"
 echo "Extracted models saved to ${grouped_json}
